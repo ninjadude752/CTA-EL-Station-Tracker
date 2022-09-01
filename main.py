39ea3d97f4477d3b3eb7dc8a3ec5506a382630f0
Spencer Blackwell <spencerblackwell02@gmail.com>
2022-09-01 14:22:18 -0500

    Edited the code to add a timer loop
    - Edited the code to add a timer loop, currently 10 seconds
    - Started working on the logic to only output the train information again if something about the information changes

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
+import collections
+from typing import List, Any
+import self
 import apiInfo
 import config
 import format
+import time
 
-dataStorage = apiInfo.apiCall(apiInfo.finalUrl)
-
-lineList = (apiInfo.line(dataStorage))
-runNumList = (apiInfo.runNum(dataStorage))
-destList = (apiInfo.dest(dataStorage))
-arrTList = (apiInfo.arrT(dataStorage))
-approachList = (apiInfo.approach(dataStorage))
-delayedList = (apiInfo.delayed(dataStorage))
-
-
+global test
+test = []
 
 def finalOutput():
-    print("Currently scheduled trains:")
     for x in range(config.amt):
-        print(format.lineDict[lineList[x]] + " line run number: " + str(runNumList[x]) + " to " + destList[x] + ", Scheduled to Arrive at: " + str(arrTList[x]),
+        print(format.lineDict[lineList[x]] + " line run number: " + str(runNumList[x]) + " to " + destList[
+            x] + ", Scheduled to Arrive at: " + str(arrTList[x]),
               format.approachDict[approachList[x]] + format.delayDict[delayedList[x]])
 
     return False
 
-finalOutput()
+
+def dataGrab():
+    dataStorage = apiInfo.apiCall(apiInfo.finalUrl)
+
+    lineList = (apiInfo.line(dataStorage))
+    runNumList = (apiInfo.runNum(dataStorage))
+    destList = (apiInfo.dest(dataStorage))
+    arrTList = (apiInfo.arrT(dataStorage))
+    approachList = (apiInfo.approach(dataStorage))
+    delayedList = (apiInfo.delayed(dataStorage))
+
+    return lineList, runNumList, destList, arrTList, approachList, delayedList
 
 
+def comparison(lineList, runNumList, destList, arrTList, approachList, delayedList):
+    if (len(test) == 0):
+        for data in runNumList:
+            test.append(data)
+    else:
+        for data in lineList:
+            if collections.Counter(test) != collections.Counter(runNumList):
+                print("Change")
+            if collections.Counter(test) == collections.Counter(runNumList):
+                print("Same")
+    return False
 
-#print(apiInfo.finalUrl)
+
+while True:
+    lineList, runNumList, destList, arrTList, approachList, delayedList = dataGrab()
+    comparison(lineList, runNumList, destList, arrTList, approachList, delayedList)
+    finalOutput()
+    time.sleep(10)
+
+# print(apiInfo.finalUrl)
 # trainDict = apiInfo.request(apiInfo.finalUrl)
 
 # print (len(trainDict))
